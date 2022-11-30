28e79b66baf9cc20e388cc36ed3dd8f9394c3340
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2022-11-30 13:02:14 +0500

    nest-practice-online-market

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CategoriesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/categories/dto/create-categories.dto.ts
@@ -0,0 +1,6 @@
+import { IsString } from "class-validator";
+
+export class CreateCategoriesDto {  
+    @IsString({message: "Category_name satr bo'lishi kerak"})
+    category_name: string
+}
